0a918c81e9644502f9bff8763fc4d912e7594220
Petrick <ptrck1110@protonmail.com>
2022-10-28 18:20:56 -0300

    Add HTTP Methods

--- apis/coffee-shop/data/products.go
+++ apis/coffee-shop/data/products.go
@@ -1,7 +1,12 @@
 package data
 
-import "time"
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
 
+// Create the structure of data for API
 type Product struct {
 	ID          int     `json:"id"`
 	Name        string  `json:"name"`
@@ -13,8 +18,31 @@ type Product struct {
 	DeleteOn    string  `json:"-"`
 }
 
-func GetProducts() []*Product {
-	return productList
+func (p *Product) FromJSON(r io.Reader) error {
+  e := json.NewDecoder(r)
+  return e.Decode(p)
+}
+
+// Collection of Product
+type Products []*Product
+
+func (p *Products) ToJSON(w io.Writer) error {
+  e := json.NewEncoder(w)
+  return e.Encode(p)
+}
+
+func GetProducts() Products {
+  return productList
+}
+
+func AddProduct(p *Product)  {
+  p.ID = getNextID()
+  productList = append(productList, p)
+}
+
+func getNextID() int {
+  lp := productList[len(productList) -1]
+  return lp.ID + 1
 }
 
 var productList = []*Product{
--- apis/coffee-shop/handlers/products.go
+++ apis/coffee-shop/handlers/products.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"coffee-shop/data"
-	"encoding/json"
 	"log"
 	"net/http"
 )
@@ -16,11 +15,52 @@ func NewProducts(l *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	lp := data.GetProducts()
-	d, err := json.Marshal(lp)
-	if err != nil {
-		http.Error(w, "Unable to marshal json", http.StatusInternalServerError)
-	}
+  // handle the request for a list of products
+  // GET
+  if r.Method == http.MethodGet {
+    p.getProducts(w, r)
+    return
+  }
 
-	w.Write(d)
+  // POST
+  if r.Method == http.MethodPost {
+    p.addProduct(w, r)
+    return
+  }
+
+  // PUT
+  if r.Method == http.MethodPut {
+    // 
+  }
+
+  // catch all
+  // if no method is satisfied return an error
+  w.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+// GET function returns the products from the data store
+func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
+  p.l.Println("Handle GET Products")
+
+  // fetch
+  lp := data.GetProducts()
+
+  // serialize the list to JSON
+  err := lp.ToJSON(w)
+	if err != nil {
+		http.Error(w, "Unable to marshal JSON", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(w http.ResponseWriter, r *http.Request) {
+  p.l.Println("Handle POST Product")
+
+  prod := &data.Product{}  
+  err := prod.FromJSON(r.Body)
+  if err != nil {
+    http.Error(w, "Unable to unmarshal JSON", http.StatusBadRequest)
+  }
+
+  p.l.Printf("Prod: %#v", prod)
+  data.AddProduct(prod)
 }
