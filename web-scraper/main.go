e9b58a2d0ca6c9eb54136231fcc31d16841c862d
Petrick <ptrck1110@protonmail.com>
2023-02-28 15:06:54 -0300

    Format code

--- web-scraper/main.go
+++ web-scraper/main.go
@@ -7,38 +7,35 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
-func main()  {
-  dt := time.Now()
+func main() {
+	dt := time.Now()
 
-  // Enable the domain.
-  c := colly.NewCollector(
-      colly.AllowedDomains("weather.com"),
-    )
+	// Enable the domain.
+	c := colly.NewCollector(
+		colly.AllowedDomains("weather.com"),
+	)
 
-  // Get Weather infos.
-  c.OnHTML(".CurrentConditions--primary--2DOqs", func(h *colly.HTMLElement) {
-    weather := h.ChildText(".CurrentConditions--tempValue--MHmYY")
-    phrase := h.ChildText(".CurrentConditions--phraseValue--mZC_p")
+	// Get Weather infos.
+	c.OnHTML(".CurrentConditions--primary--2DOqs", func(h *colly.HTMLElement) {
+		weather := h.ChildText(".CurrentConditions--tempValue--MHmYY")
+		phrase := h.ChildText(".CurrentConditions--phraseValue--mZC_p")
 
-    fmt.Println("")
-    fmt.Println("🌤️", weather)
-    fmt.Println(phrase)
-    fmt.Println("Curitiba -", dt.Format("02/01/2006 15:04"))
+		fmt.Println("")
+		fmt.Println("🌤️", weather)
+		fmt.Println(phrase)
+		fmt.Println("Curitiba -", dt.Format("02/01/2006 15:04"))
+	})
 
-  })
+	// Get the HTTP status code.
+	// c.OnResponse(func(r *colly.Response) {
+	// 	fmt.Println("Status Code:", r.StatusCode)
+	// 	fmt.Println("Status Code:", r.Request.URL)
+	// })
 
-  // Get the HTTP status code.
-  // c.OnResponse(func(r *colly.Response) {
-  //   fmt.Println("Status Code:", r.StatusCode) 
-  //   fmt.Println("Status Code:", r.Request.URL) 
-  // })
+	// c.OnRequest(func(r *colly.Request) {
+	// 	fmt.Println("Visiting...")
+	// })
 
-  // c.OnRequest(func(r *colly.Request) {
-  //   fmt.Println("Visiting...")
-  // })
-
-
-  // Define the url and acess.
-  c.Visit("https://weather.com/pt-BR/clima/hoje/l/cd456e246b710e10cd019303ee89dd7486d87c3f89d1b01b0c96e0929fe4b296")
-
+	// Define the url and acess.
+	c.Visit("https://weather.com/pt-BR/clima/hoje/l/cd456e246b710e10cd019303ee89dd7486d87c3f89d1b01b0c96e0929fe4b296")
 }
